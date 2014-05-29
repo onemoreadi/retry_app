ed67a15079d88026eb2cb7e91b34b7f30f276782
aditya_git <adityakr6124@gmail.com>
2014-05-29 23:42:36 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RetryApp::Application.config.secret_key_base = 'b0529a8cd1b94658beece18d3e5568815ce5bd6330e828c9d58aae7a9c5f4d5d7d7be68eba1bd6d9b908c0d5ea64c3c99847859eb95db72d7c807e750891b803'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RetryApp::Application.config.session_store :cookie_store, key: '_retry_app_session'
